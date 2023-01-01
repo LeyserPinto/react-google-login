a389a4c5b4d432b55b637c2e5a569e4b21950968
Leyser Pinto <leyserpinto3@gmail.com>
2023-01-01 17:24:08 -0300

    react google #2

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,8 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+
+
 import Profile from './profile';
 
 const router = createBrowserRouter([
@@ -22,7 +24,6 @@ const router = createBrowserRouter([
 ]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-
     <RouterProvider router={router} />
   </React.StrictMode>,
 )
